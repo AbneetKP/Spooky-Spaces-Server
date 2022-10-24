b31fd98a5591cd76a0a8a99be77991a9699a026a
Abneet K Pabyal <Abneet.P@gmail.com>
2022-10-24 16:19:50 -0600

    imported and exported files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { router } from "./routers/rolldice";
 
 const app = express();
 const port = 3000;
+app.use("/rollDice", router);
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
